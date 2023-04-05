fea72a0d6a8fd38c9235b9580a81c12a58699e0d
lucky1313 <lucky1313@gmail.com>
2023-04-05 14:09:12 -0700

    C - Recursion updated

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*pow_recursion-returns the value of x raised to the power of y
+*@x: value to riase
+*@y: power
+*
+*Return: result of the power
+**/
+int_pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	return (-1);
+
+	if (y == 0)
+	return (1);
+{
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
+}
